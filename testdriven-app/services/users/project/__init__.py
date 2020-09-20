d23540d624cb77e113424d61b6182cc462eb429a
felkost <felkost@ukr.net>
2020-09-20 19:13:43 +0300

    Add dockerfile for activation enviroment OSMNX in the Anaconda

--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -1,16 +1,25 @@
+import os
 from flask import Flask, jsonify
+import osmnx as ox
 
 # instantiate the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
+    G = ox.graph_from_place('Kovel, Volyn, Ukraine', network_type='drive')
+    G_proj = ox.project_graph(G)
+    nodes_proj = ox.graph_to_gdfs(G_proj, edges=False)
+    graph_area_m = nodes_proj.unary_union.convex_hull.area
     return jsonify({
         'status': 'success',
         'message': 'pong!',
-        'message_my': 'Nice to meet you!'
+        'message_my': 'Nice to meet you! Is my Docker',
+        'ox.__version__': ox.__version__,
+        'graph_area_m': graph_area_m
     })
