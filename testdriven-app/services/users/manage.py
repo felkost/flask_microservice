0d5bb0bcedeb5af36506e564d8656906b847ad2a
felkost <felkost@ukr.net>
2020-09-20 21:28:10 +0300

    Fix comments in manage.py

--- testdriven-app/services/users/manage.py
+++ testdriven-app/services/users/manage.py
@@ -1,3 +1,12 @@
+"""
+Для запуска скрипта (локально, не из Dockerfile) необходимо:
+1) перейти в каталог testdriven-app/services/users
+2) export FLASK_APP=project/__init__.py
+3) export FLASK_DEBUG=1
+4) выполнить  python manage.py run
+результат открыть в браузере http://localhost:5000/users/ping
+PS: для образа из Docker используется порт 5001, поэтому результ открыть по адресу http://localhost:5001/users/ping
+"""
 from flask.cli import FlaskGroup
 from project import app
 
